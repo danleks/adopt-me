bbbe28bc60db1863fc36a97b3a8b07f9bc71d48e
danleks <danleks@pop-os.localdomain>
2022-05-02 22:07:38 +0200

    update the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { render } from "react-dom";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
