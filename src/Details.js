41c653df9048fe1c902267a5dbf91338f4222880
danleks <danleks@pop-os.localdomain>
2022-05-04 21:09:07 +0200

    update the app

--- src/Details.js
+++ src/Details.js
@@ -3,9 +3,10 @@ import { Component } from "react";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true }; // due to installing @babel/plugin-proposal-class-properties
+  state = { loading: true, showModal: false }; // due to installing @babel/plugin-proposal-class-properties
 
   async componentDidMount() {
     const res = await fetch(
@@ -17,12 +18,16 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
   render() {
     if (this.state.loading) {
       return <h2>Loading ... </h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -34,11 +39,26 @@ class Details extends Component {
         </h2>
         <ThemeContext.Consumer>
           {([theme]) => (
-            <button style={{ backgroundColor: theme }}>Adopot {name}</button>
+            <button
+              style={{ backgroundColor: theme }}
+              onClick={this.toggleModal}
+            >
+              Adopot {name}
+            </button>
           )}
         </ThemeContext.Consumer>
-
         <p>{description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h2>Would you like to adopt {name} ? </h2>
+              <div className="buttons">
+                <a href="https://bit.ly/pet-adopt">Yes</a>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     );
   }
